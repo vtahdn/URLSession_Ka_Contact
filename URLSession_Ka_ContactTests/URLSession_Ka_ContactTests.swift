6b9309898a5b41d623e3a2a45d5f0100db96b702
Viet Asc <vtahdn@gmail.com>
2018-12-31 15:55:24 +0700

    Initial Commit

---
+++ URLSession_Ka_ContactTests/URLSession_Ka_ContactTests.swift
@@ -0,0 +1,34 @@
+//
+//  URLSession_Ka_ContactTests.swift
+//  URLSession_Ka_ContactTests
+//
+//  Created by Viet Asc on 12/31/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import XCTest
+@testable import URLSession_Ka_Contact
+
+class URLSession_Ka_ContactTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
